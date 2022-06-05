8c1a327856bd2366090a473ff3d837e49278b387
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-05 10:56:37 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
-}
+    var x = document.getElementById("input").value;
+   return document.getElementById("value").innerHTML = "You wrote: " + x;
+  }
+
+  document.getElementById("input").addEventListener("input", addingEventListener, );
